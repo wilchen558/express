ea68ef9466321d53f91058599b4753e8ee873209
Tobias Wilchen <twp@peytz.dk>
2017-04-01 13:29:14 +0200

    added console.log

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-  res.send('Hello  World!')
+  res.send('Hello  World!');
+  console.log("log test");
 });
 
 app.listen(app.get('port'), function() {
