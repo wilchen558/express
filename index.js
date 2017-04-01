f7ac86054264390e556c8d1c94884975e3208a31
Tobias Wilchen <twp@peytz.dk>
2017-04-01 14:04:42 +0200

    added bodyparser

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ var bodyParser = require('body-parser')
 
 var app = express();
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
+app.use(bodyParser.raw()); 
 
 app.set('port', (process.env.PORT || 5000));
 
